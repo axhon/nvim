dd560eebe148bf8c340e949b5c04236386ca87e4
Axhon <axhonruiz@gmail.com>
2023-10-18 23:00:15 -0400

    doot doot

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -1,12 +1,12 @@
 return {
   {
     "hrsh7th/nvim-cmp",
-    dependencies = { "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline" },
+    dependencies = { "hrsh7th/cmp-path", "hrsh7th/cmp-cmdline", "hrsh7th/cmp-emoji" },
     ---@param opts cmp.ConfigSchema
     opts = function(_, opts)
       local cmp = require("cmp")
 
-      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "path" } }))
+      opts.sources = cmp.config.sources(vim.list_extend(opts.sources, { { name = "path" }, { name = "emoji" } }))
 
       -- only set this up for cmdline
       cmp.setup.cmdline(":", {
