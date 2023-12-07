b7ed88297965c13d12895942ccaa9b1ab6019d14
Axhon <axhonruiz@gmail.com>
2023-12-07 13:54:41 -0500

    remove neotree

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,11 +3,28 @@ return {
   {
     "telescope.nvim",
     dependencies = {
-      "nvim-telescope/telescope-fzf-native.nvim",
-      build = "make",
-      config = function()
-        require("telescope").load_extension("fzf")
-      end,
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+        config = function()
+          require("telescope").load_extension("fzf")
+        end,
+      },
+      -- project management
+      {
+        "ahmedkhalf/project.nvim",
+        opts = {},
+        event = "VeryLazy",
+        config = function(_, opts)
+          require("project_nvim").setup(opts)
+          require("lazyvim.util").on_load("telescope.nvim", function()
+            require("telescope").load_extension("projects")
+          end)
+        end,
+        keys = {
+          { "<leader>fp", "<Cmd>Telescope projects<CR>", desc = "Projects" },
+        },
+      },
     },
   },
 }
