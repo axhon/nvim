9885f16a5003f1c7725f6562560cd494871f4a05
Axhon <axhonruiz@gmail.com>
2024-01-04 21:04:08 -0500

    set up telekasten keybinds. sick

--- lua/plugins/telekasten.lua
+++ lua/plugins/telekasten.lua
@@ -5,5 +5,71 @@ return {
     opts = {
       home = vim.fn.expand("~/notes"),
     },
+    keys = function()
+      local telekasten = require("telekasten")
+      return {
+        {
+          "<leader>zfn",
+          function()
+            telekasten.find_notes()
+          end,
+          desc = "Find Notes",
+        },
+        {
+          "<leader>zfd",
+          function()
+            telekasten.find_daily_notes()
+          end,
+          desc = "Find Daily Notes",
+        },
+        {
+          "<leader>znn",
+          function()
+            telekasten.new_note()
+          end,
+          desc = "Create new note",
+        },
+        {
+          "<leader>zN",
+          function()
+            telekasten.new_note()
+          end,
+          desc = "Create new note",
+        },
+        {
+          "<leader>zgt",
+          function()
+            telekasten.goto_today()
+          end,
+          desc = "Open Today's Note",
+        },
+        {
+          "<leader>zsn",
+          function()
+            telekasten.search_notes()
+          end,
+          desc = "Search for notes containing input",
+        },
+        {
+          "<leader>zst",
+          function()
+            telekasten.show_tags()
+          end,
+          desc = "Search for notes containing tags",
+        },
+        {
+          "<leader>ztt",
+          function()
+            telekasten.toggle_todo()
+          end,
+          desc = "Toggle Todo",
+        },
+        {
+          "<leader>z",
+          name = "+Telekasten",
+          desc = "Telekasten",
+        },
+      }
+    end,
   },
 }
