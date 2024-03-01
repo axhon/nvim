fab2b32bd8b4a835d84ab2b6c581591372e347ee
Axhon <axhonruiz@gmail.com>
2024-03-01 18:32:38 -0500

    update deps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,19 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+vim.keymap.set("n", "<leader>e", function()
+  -- get the location of the current buffer
+  local dir = vim.fn.expand("%:p:h")
+
+  local chosenFile = vim.fn.input({
+    prompt = "Choose file to edit\n",
+    default = dir,
+    completion = "file",
+    cancelreturn = "",
+  })
+
+  if #chosenFile > 0 then
+    vim.cmd.edit(chosenFile)
+  end
+end)
